4a857e108d081eccf54d75bfb31f3c8f24520d43
SimonEggert <simon-eggert@web.de>
2021-03-14 01:07:18 +0100

    Add Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,15 +4,66 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <Label>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <SelectEl value={value} onChange={onChange}>
+          {children}
+        </SelectEl>
+      </Label>
+      {displayedValue}
+      <Chevron id="chevron-down" size={24} />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  width: fit-content;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+  background: ${COLORS.transparentGray15};
+
+  &:focus-within {
+    outline: 2px solid;
+  }
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`;
+
+const SelectEl = styled.select`
+  width: 100%;
+  height: 100%;
+`;
+
+const Label = styled.label`
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Chevron = styled(Icon)`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  pointer-events: none;
+`;
 
 export default Select;
